7afd46f9367d8703b2d0ada0e3912bcd6d74966c
andika350 <andikapangestu79@gmail.com>
2021-09-02 03:55:47 +0700

    initial commit

---
+++ app.js
@@ -0,0 +1,71 @@
+const tagsEl = document.getElementById('tags');
+const textArea = document.getElementById('text-area');
+
+
+
+textArea.focus();
+
+textArea.addEventListener('keyup', (e) => {
+    createTags(e.target.value);
+
+    if(e.key === 'Enter') {
+        setTimeout(() => {
+            e.target.value =''
+        },100);
+
+        randomPicker();
+    }
+})
+
+
+
+function createTags(input) {
+    const tags = input.split(',').filter(tag => tag.trim() !== '').map(tag => tag.trim());
+    
+    tagsEl.innerHTML = '';
+
+    tags.forEach(tag => {
+        const tagEl = document.createElement('span');
+        tagEl.classList.add('tag');
+        tagEl.innerText = tag;
+        tagsEl.appendChild(tagEl);
+    })
+}
+
+function randomPicker() {
+    const times = 20;
+
+    const interval = setInterval(() => {
+        const randomTag = pickRandomTag()
+
+        highlight(randomTag)
+
+        setTimeout(() => {
+            unHighlight(randomTag)
+        },100);
+    }, 100);
+
+    setTimeout(() => {
+        clearInterval(interval)
+
+        setTimeout(() => {
+            const randomTag = pickRandomTag();
+
+            highlight(randomTag);
+        },100);
+
+    },times * 100);
+}
+
+function pickRandomTag() {
+    const tags = document.querySelectorAll('.tag')
+    return tags[Math.floor(Math.random()*tags.length)]
+}
+
+function highlight(tag) {
+    tag.classList.add('highlight');
+}
+
+function unHighlight(tag) {
+    tag.classList.remove('highlight'); 
+}
